73902128371a2f28ccb8edb9320b9e4cb6a774d0
ASLAN USSAYEV <usprof777@gmail.com>
2015-11-22 17:37:33 -0600

    Injected the [] into the controller to load data from the server and tested it

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,18 +10,19 @@ describe('PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
+  it('should filter the phone list as a user types into the search box', function() {
+
     var phoneList = element.all(by.repeater('phone in phones'));
     var query = element(by.model('query'));
 
-    it('should filter the phone list as a user types into the search box', function() {
-      expect(phoneList.count()).toBe(3);
+      expect(phoneList.count()).toBe(20);
 
       query.sendKeys('nexus');
       expect(phoneList.count()).toBe(1);
 
       query.clear();
       query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+      expect(phoneList.count()).toBe(8);
     });
 
 	it('should be possible to control phone order via the drop down select box', function() {
