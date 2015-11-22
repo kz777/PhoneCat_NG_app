78647987c03e066e7bee6470ca20b15d644f3e6a
ASLAN USSAYEV <usprof777@gmail.com>
2015-11-22 01:42:40 -0600

    Added a search box and end-to-end test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,34 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('PhoneCat App', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
+
+    it('should filter the phone list as a user types into the search box', function() {
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
+
+    it('should display the current filter value in the title bar', function() {
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    });
   });
-
 });
