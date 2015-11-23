4baf30314f0136b70ce67b9402969be4771b0ecc
ASLAN USSAYEV <usprof777@gmail.com>
2015-11-22 19:04:10 -0600

    Add phone images,links,some styling and tested links (end2end).

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -50,5 +50,15 @@ describe('PhoneCat App', function() {
 	    "Motorola XOOM\u2122 with Wi-Fi"
 	  ]);
 	});
+
+
+	it('should render phone specific links', function() {
+      var query = element(by.model('query'));
+      query.sendKeys('nexus');
+      element.all(by.css('.phones li a')).first().click();
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url).toBe('/phones/nexus-s');
+      });
+    });
   });
 });
